3a219abf1269c6452413b954e8517daa78a14824
Prerak Mathur <prerakrk7@gmail.com>
2021-11-23 21:03:56 -0800

    added cart login register pages

--- server.js
+++ server.js
@@ -9,15 +9,25 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
 
 app.use(expressLayout);
 
 app.set("views", path.join(__dirname, "resources/views"));
 
 app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/cart", function (req, res) {
+  res.render("customers/cart");
+});
+app.get("/login", function (req, res) {
+  res.render("auth/login");
+});
+app.get("/register", function (req, res) {
+  res.render("auth/register");
+});
+
 
 app.listen(PORT, () => {
   console.log(
